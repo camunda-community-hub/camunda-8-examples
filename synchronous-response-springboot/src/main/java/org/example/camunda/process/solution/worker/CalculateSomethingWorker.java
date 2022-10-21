1c79c1cae342d957c549a247937844d4ab8fb613
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-10-21 16:30:14 +0200

    add output about worker invocation

--- synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/worker/CalculateSomethingWorker.java
+++ synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/worker/CalculateSomethingWorker.java
@@ -1,6 +1,8 @@
 package org.example.camunda.process.solution.worker;
 
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -8,9 +10,18 @@ import java.util.Map;
 
 @Component
 public class CalculateSomethingWorker {
+  
+  private static final Logger LOG = LoggerFactory.getLogger(CalculateSomethingWorker.class);
+
 
   @JobWorker
   public Map<String, Object> calculateSomething() {
+    try {
+      LOG.info("Cause a 5 seconds delay");
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      LOG.error("Thread sleep got interrupted: {}", e.getMessage());
+    }
     return Collections.singletonMap("response", "The response is - of course - 42");
   }
 }
