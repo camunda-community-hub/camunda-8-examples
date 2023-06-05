30f5f85eeb86014ca91798d13648e53995a3b62e
Jonathan Lukas <jonathan.lukas@camunda.com>
2023-06-05 09:53:48 +0200

    formatter

--- large-multi-instance-example/src/test/java/com/camunda/consulting/MultiInstanceTest.java
+++ large-multi-instance-example/src/test/java/com/camunda/consulting/MultiInstanceTest.java
@@ -2,37 +2,35 @@ package com.camunda.consulting;
 
 import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.*;
 
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import java.time.Duration;
 import java.util.Map;
-
 import org.camunda.community.process_test_coverage.junit5.platform8.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
-import io.camunda.zeebe.spring.test.ZeebeSpringTest;
-
 @SpringBootTest
 @ZeebeSpringTest
 @ExtendWith(ProcessEngineCoverageExtension.class)
 public class MultiInstanceTest {
-  
-  @Autowired
-  ZeebeClient client;
-  
+
+  @Autowired ZeebeClient client;
+
   @Test
   public void testCampaignWithFewElements() {
-    ProcessInstanceEvent processInstance = client
-        .newCreateInstanceCommand()
-        .bpmnProcessId("campaignProcess")
-        .latestVersion()
-        .variables(Map.of("campaignId", "1"))
-        .send()
-        .join();
-    
-    waitForProcessInstanceCompleted(processInstance);
-  }
+    ProcessInstanceEvent processInstance =
+        client
+            .newCreateInstanceCommand()
+            .bpmnProcessId("campaignProcess")
+            .latestVersion()
+            .variables(Map.of("campaignId", "1"))
+            .send()
+            .join();
 
+    waitForProcessInstanceCompleted(processInstance, Duration.ofMinutes(1));
+  }
 }
--- large-multi-instance-example/src/test/java/com/camunda/consulting/SimpleMultiInstanceTest.java
+++ large-multi-instance-example/src/test/java/com/camunda/consulting/SimpleMultiInstanceTest.java
@@ -2,8 +2,12 @@ package com.camunda.consulting;
 
 import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.*;
 
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import java.time.Duration;
 import java.util.List;
-
 import org.camunda.community.process_test_coverage.junit5.platform8.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,33 +16,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
-import io.camunda.zeebe.process.test.assertions.BpmnAssert;
-import io.camunda.zeebe.spring.test.ZeebeSpringTest;
-
 @SpringBootTest
 @ZeebeSpringTest
 @ExtendWith(ProcessEngineCoverageExtension.class)
 public class SimpleMultiInstanceTest {
-  
+
   private static final Logger LOG = LoggerFactory.getLogger(SimpleMultiInstanceTest.class);
 
-  @Autowired
-  ZeebeClient client;
-  
+  @Autowired ZeebeClient client;
+
   @Test
   public void runCallActivity() {
     LOG.info("Call activity");
-    ProcessInstanceEvent processInstance = client
-        .newCreateInstanceCommand()
-        .bpmnProcessId("MICallActivityProcess")
-        .latestVersion()
-        .send()
-        .join();
-    
-    waitForProcessInstanceCompleted(processInstance);
+    ProcessInstanceEvent processInstance =
+        client
+            .newCreateInstanceCommand()
+            .bpmnProcessId("MICallActivityProcess")
+            .latestVersion()
+            .send()
+            .join();
+
+    waitForProcessInstanceCompleted(processInstance, Duration.ofMinutes(1));
     BpmnAssert.assertThat(processInstance).hasVariableWithValue("resultList", List.of(65, 66, 67));
   }
-
 }
