dce9c41d25b1ea6019c2f93d4861abea636d106b
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2023-06-15 09:27:18 +0200

    Add tests for all models

--- large-multi-instance-example/src/test/java/com/camunda/consulting/MultiInstanceTest.java
+++ large-multi-instance-example/src/test/java/com/camunda/consulting/MultiInstanceTest.java
@@ -1,5 +1,6 @@
 package com.camunda.consulting;
 
+import static io.camunda.zeebe.process.test.assertions.BpmnAssert.*;
 import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.*;
 
 import io.camunda.zeebe.client.ZeebeClient;
@@ -21,16 +22,32 @@ public class MultiInstanceTest {
   @Autowired ZeebeClient client;
 
   @Test
+  public void testCampaignProcess() {
+    runProcessAndTestCompleted("CampaignProcess", "2");
+  }
+
+  @Test
+  public void testLargeCampaignProcess() {
+    runProcessAndTestCompleted("LargeCampaignProcess", "3");
+  }
+
+  @Test
   public void testHugeCampaignWithFewElements() {
+    runProcessAndTestCompleted("HugeCampaignProcess", "1");
+  }
+
+  protected void runProcessAndTestCompleted(String processId, String campaignId) {
     ProcessInstanceEvent processInstance =
         client
             .newCreateInstanceCommand()
-            .bpmnProcessId("HugeCampaignProcess")
+            .bpmnProcessId(processId)
             .latestVersion()
-            .variables(Map.of("campaignId", "1"))
+            .variables(Map.of("campaignId", campaignId))
             .send()
             .join();
 
     waitForProcessInstanceCompleted(processInstance, Duration.ofMinutes(1));
+
+    assertThat(processInstance).isCompleted();
   }
 }
