f106adbcdc665953669134381698999a94437223
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-04-12 11:49:14 +0200

    formatter

--- synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/worker/CalculateSomethingWorker.java
+++ synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/worker/CalculateSomethingWorker.java
@@ -1,16 +1,15 @@
 package org.example.camunda.process.solution.worker;
 
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import java.util.Collections;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.Map;
-
 @Component
 public class CalculateSomethingWorker {
-  
+
   private static final Logger LOG = LoggerFactory.getLogger(CalculateSomethingWorker.class);
 
   @JobWorker
