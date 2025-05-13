e8616f67f89d72269296b03c065436af85288e2f
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-05-13 14:40:09 +0200

    made test independent from httpbin.org (#699)
    * made test independent from httpbin.org
    
    * format

--- extended-connector-runtime/src/test/java/com/camunda/consulting/example/AppTest.java
+++ extended-connector-runtime/src/test/java/com/camunda/consulting/example/AppTest.java
@@ -11,11 +11,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @CamundaSpringProcessTest
+@Testcontainers
 public class AppTest {
   @Autowired ZeebeClient zeebeClient;
+
+  @Container
+  private final GenericContainer<?> httpbin =
+      new GenericContainer<>("kennethreitz/httpbin").withExposedPorts(80);
 
   @BeforeEach
   public void setup() {
@@ -24,12 +32,14 @@ public class AppTest {
 
   @Test
   void shouldExecute() {
+    Integer mappedPort = httpbin.getMappedPort(80);
     Duration nextExecutionBackoff = Duration.ofMinutes(20);
     ProcessInstanceResult result =
         zeebeClient
             .newCreateInstanceCommand()
             .bpmnProcessId("test")
             .latestVersion()
+            .variable("host", "http://localhost:" + mappedPort)
             .withResult()
             .send()
             .join();
