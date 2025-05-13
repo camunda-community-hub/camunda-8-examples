4c7847c0072621b7f4d6d61774aa1d6368a3a90a
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-05-13 15:53:35 +0200

    use spring sdk in version 8.7 (#702)

--- large-multi-instance-example/src/test/java/com/camunda/consulting/SimpleMultiInstanceTest.java
+++ large-multi-instance-example/src/test/java/com/camunda/consulting/SimpleMultiInstanceTest.java
@@ -1,24 +1,19 @@
 package com.camunda.consulting;
 
-import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.*;
+import static io.camunda.process.test.api.CamundaAssert.*;
 
+import io.camunda.process.test.api.CamundaSpringProcessTest;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
-import io.camunda.zeebe.process.test.assertions.BpmnAssert;
-import io.camunda.zeebe.spring.test.ZeebeSpringTest;
-import java.time.Duration;
 import java.util.List;
-import org.camunda.community.process_test_coverage.junit5.platform8.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ZeebeSpringTest
-@ExtendWith(ProcessEngineCoverageExtension.class)
+@CamundaSpringProcessTest
 public class SimpleMultiInstanceTest {
 
   private static final Logger LOG = LoggerFactory.getLogger(SimpleMultiInstanceTest.class);
@@ -36,7 +31,6 @@ public class SimpleMultiInstanceTest {
             .send()
             .join();
 
-    waitForProcessInstanceCompleted(processInstance, Duration.ofMinutes(1));
-    BpmnAssert.assertThat(processInstance).hasVariableWithValue("resultList", List.of(65, 66, 67));
+    assertThat(processInstance).hasVariable("resultList", List.of(65, 66, 67));
   }
 }
