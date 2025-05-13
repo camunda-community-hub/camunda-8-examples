9b6dc81f3de49d94fd7e1326853399fafe443ac0
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-05-13 17:33:18 +0200

    move sync response to 8.7 (#707)

--- synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/SyncResponseProcessApplication.java
+++ synchronous-response-springboot/src/main/java/org/example/camunda/process/solution/SyncResponseProcessApplication.java
@@ -1,12 +1,10 @@
 package org.example.camunda.process.solution;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 @Deployment(resources = "classpath*:/models/*.*")
 public class SyncResponseProcessApplication {
 
--- synchronous-response-springboot/src/test/java/org/example/camunda/process/solution/AppTest.java
+++ synchronous-response-springboot/src/test/java/org/example/camunda/process/solution/AppTest.java
@@ -2,14 +2,14 @@ package org.example.camunda.process.solution;
 
 import static org.assertj.core.api.Assertions.*;
 
-import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import io.camunda.process.test.api.CamundaSpringProcessTest;
 import org.example.camunda.process.solution.facade.ProcessController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ZeebeSpringTest
+@CamundaSpringProcessTest
 public class AppTest {
 
   @Autowired ProcessController processController;
