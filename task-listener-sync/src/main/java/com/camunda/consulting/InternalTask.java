f7e0122209ad942c7259bec953cdf070fe442ecc
Jonathan Lukas <jonathan.lukas@camunda.com>
2026-01-12 15:26:22 +0100

    fix: set up formatting

--- task-listener-sync/src/main/java/com/camunda/consulting/InternalTask.java
+++ task-listener-sync/src/main/java/com/camunda/consulting/InternalTask.java
@@ -3,9 +3,16 @@ package com.camunda.consulting;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public record InternalTask(Map<String, Object> variables, Supplier<Object> formSupplier, State state,
-                           SyncType syncType) {
-  public enum State {CREATED, COMPLETED, CANCELED}
+public record InternalTask(
+    Map<String, Object> variables, Supplier<Object> formSupplier, State state, SyncType syncType) {
+  public enum State {
+    CREATED,
+    COMPLETED,
+    CANCELED
+  }
 
-  public enum SyncType {REACTIVE, POLLING}
+  public enum SyncType {
+    REACTIVE,
+    POLLING
+  }
 }
--- task-listener-sync/src/main/java/com/camunda/consulting/TaskDto.java
+++ task-listener-sync/src/main/java/com/camunda/consulting/TaskDto.java
@@ -1,8 +1,6 @@
 package com.camunda.consulting;
 
 import com.camunda.consulting.InternalTask.State;
-
 import java.util.Map;
 
-public record TaskDto(
-    Map<String, Object> variables, Object formSchema, State status) {}
+public record TaskDto(Map<String, Object> variables, Object formSchema, State status) {}
