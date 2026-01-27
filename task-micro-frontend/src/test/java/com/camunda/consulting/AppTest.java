e85eaafe4d234957ed8d5b1e280c3bff73e0a504
Jonathan Lukas <jonathan.lukas@camunda.com>
2026-01-27 22:03:44 +0100

    removed tasklist client (#1045)

--- task-micro-frontend/src/test/java/com/camunda/consulting/AppTest.java
+++ task-micro-frontend/src/test/java/com/camunda/consulting/AppTest.java
@@ -1,9 +1,11 @@
 package com.camunda.consulting;
 
+import io.camunda.process.test.api.CamundaSpringProcessTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@CamundaSpringProcessTest
 public class AppTest {
 
   @Test
